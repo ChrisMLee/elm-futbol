cb8791dcfd44daff48a13be849934eac633c430b
Christopher Lee <cristovao.m.lee@gmail.com>
2016-11-22 22:38:05 -0500

    fixture view working

--- elm-futbol.elm
+++ elm-futbol.elm
@@ -12,14 +12,14 @@ main =
         }
 
 -- MODEL
-model =
-  {
-    leagues = [ { name = "Premier League", checked = False, id = 426}
-              , { name = "La Liga", checked = False, id = 436}
-              , { name = "Bundesliga", checked = False, id = 430}
-              , { name = "Serie A", checked = False, id = 438}
-    ]
-  }
+--model =
+--  {
+--    leagues = [ { name = "Premier League", checked = False, id = 426}
+--              , { name = "La Liga", checked = False, id = 436}
+--              , { name = "Bundesliga", checked = False, id = 430}
+--              , { name = "Serie A", checked = False, id = 438}
+--    ]
+--  }
 
 --  , { name = "Ligue 1", checked = False, id = 434}
 --  , { name = "Champions League", checked = False, id = 440}
@@ -27,6 +27,7 @@ model =
 type alias Model =
     { 
       leagues : List League
+    , fixtures: List Fixture
     }
 
 type alias League =
@@ -35,13 +36,60 @@ type alias League =
     , id: Int
     }
 
+type alias Fixture =
+    { date : String
+    , status : String
+    , matchday : Int
+    , homeTeamName : String
+    , awayTeamName : String
+    , result : FixtureResult
+    , competition: Int
+    }
+
+type alias FixtureResultHalfTime =
+    { goalsHomeTeam : Int
+    , goalsAwayTeam : Int
+    }
+
+type alias FixtureResult =
+    { goalsHomeTeam : Int
+    , goalsAwayTeam : Int
+    }
+
+
 init : (Model, Cmd Msg)
 init =
-  (Model [ { name = "Premier League", checked = False, id = 426}
-              , { name = "La Liga", checked = False, id = 436}
-              , { name = "Bundesliga", checked = False, id = 430}
-              , { name = "Serie A", checked = False, id = 438}
-    ], Cmd.none)
+  ((Model [ { name = "Premier League", checked = True, id = 426}
+              , { name = "La Liga", checked = True, id = 436}
+              , { name = "Bundesliga", checked = True, id = 430}
+              , { name = "Serie A", checked = True, id = 438}
+    ] [ {  date = "2016-10-15T14:00:00Z" 
+         , status = "FINISHED" 
+         , matchday = 8 
+         , homeTeamName = "Stoke City FC" 
+         , awayTeamName = "Sunderland AFC"
+         , result = { goalsHomeTeam = 2 , goalsAwayTeam = 0 }
+         , competition = 426
+        }
+      , {  date = "2016-10-15T13:00:00Z" 
+         , status = "FINISHED" 
+         , matchday = 8 
+         , homeTeamName = "SSC Napoli" 
+         , awayTeamName = "AS Roma"
+         , result = { goalsHomeTeam = 1 , goalsAwayTeam = 3 }
+         , competition = 438
+        }
+      , 
+        {  date = "2016-10-15T14:15:00Z" 
+         , status = "FINISHED" 
+         , matchday = 8 
+         , homeTeamName = "FC Barcelona" 
+         , awayTeamName = "RC Deportivo La Coruna"
+         , result = { goalsHomeTeam = 4 , goalsAwayTeam = 0 }
+         , competition = 436
+        }
+
+        ]), Cmd.none)
 
 -- The exclamation point in model ! [] is just a short-hand function for (model, Cmd.batch []), 
 -- which is the type returned from typical update statements
@@ -69,22 +117,46 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
+    --let
+    --  checkedLeagues = List.map (\x -> x.competition) << List.filter (\x -> x.checked) model.leagues 
+    --  visibleFixtures = List.filter (\x -> ) model.fixtures
+    --in 
     div []
         [ 
           checkboxList model
+          , fixtureList model
         ]
 
-toString : Bool -> String 
-toString x = if x then "True" else "False"
+boolToString : Bool -> String 
+boolToString x = if x then " True" else " False"
 
 --onClick (Check todo.id (not todo.completed))
 leagueView : League -> Html Msg
 leagueView league =
     div[][
-      label [] [ text league.name, text (toString league.checked) ]
-      , input [ type' "checkbox", onClick (Check league.id (not league.checked)) ] []
+      label [] [ text league.name, text (boolToString league.checked) ]
+    , input [ type' "checkbox", onClick (Check league.id (not league.checked)) ] []
     ]
-    
+
+fixtureList : Model -> Html Msg
+fixtureList model =
+  div []
+    (List.map fixtureView model.fixtures)
+
+fixtureView: Fixture -> Html Msg
+fixtureView fixture =
+    div [][
+      div[][
+        text fixture.date
+      ]
+    , div[][
+        text fixture.homeTeamName, text <| toString fixture.result.goalsHomeTeam
+      ]
+    , div[][
+         text fixture.awayTeamName, text <| toString fixture.result.goalsAwayTeam
+      ]
+    ]
+
 
 checkboxList : Model -> Html Msg
 checkboxList model =
